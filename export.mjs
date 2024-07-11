b95effc71023d14d2921869a3b1ad6abfda4bb41
Andrii Kostenko <andrey@kostenko.name>
2024-07-11 21:06:14 +0300

    v0.2

--- export.mjs
+++ export.mjs
@@ -1,6 +1,8 @@
-#!env zx
+#!/usr/bin/env zx
 import { UTCDate } from "@date-fns/utc";
 import { endOfQuarter, startOfQuarter, subMonths, getUnixTime } from 'date-fns';
 const sq = startOfQuarter(subMonths(new UTCDate(), 3))
 const eq = endOfQuarter(subMonths(new UTCDate(), 3))
-await $`stripe reporting report_runs create --report-type=tax.filing.itemized.3 --parameters.interval-end=${getUnixTime(eq)} --parameters.interval-start=${getUnixTime(sq)}`
+const pendingReportInfo = JSON.parse(await $`stripe reporting report_runs create --live --report-type=tax.filing.itemized.3 --parameters.interval-end=${getUnixTime(eq)} --parameters.interval-start=${getUnixTime(sq)}`)
+const reportInfo = JSON.parse(await $`stripe reporting report_runs retrieve --live ${pendingReportInfo.id}`)
+console.log(`Now you need to run curl -H 'Authorization: Bearer rk_live_XXXXXXXXXXXXX' ${reportInfo.result.url} > output.csv`);
--- process.mjs
+++ process.mjs
@@ -1,16 +1,8 @@
-PUT https://tax-oss.mof.gov.cy/api/vatreturn/{{return_id}}/save/temporary
-Accept: application/json, text/plain, */*
-Connection: keep-alive
-Referer: https://tax-oss.mof.gov.cy/vat-return/edit/12243
-Sec-Fetch-Dest: empty
-Sec-Fetch-Mode: cors
-Sec-Fetch-Site: same-origin
-User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36
-sec-ch-ua: "Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"
-sec-ch-ua-mobile: ?0
-sec-ch-ua-platform: "macOS"
-x-access-token: {{access_token}}
-{
+#!/usr/bin/env zx
+const res = JSON.parse(await $`mlr --icsv --ojson cut -f country_code,filing_currency,filing_total_taxable_sales,filing_tax_payable then filter '$filing_total_taxable_sales>0' then stats1 -f filing_total_taxable_sales,filing_tax_payable -g country_code -a sum output.csv`)
+const resMap = Object.fromEntries(res.map(el => [el.country_code, el.filing_total_taxable_sales_sum]))
+const vatMap = Object.fromEntries(res.map(el => [el.country_code, el.filing_tax_payable_sum]))
+const taxJSON = {
   "supplies_from_member_state_of_identification": [
     {
       "vat_return_supply_id": 0,
@@ -21,8 +13,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 190,
       "vat_rate": 20,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0,
+      "taxable_amount": resMap["AT"],
+      "vat_amount": vatMap["AT"],
       "vat_code": "STANDARD"
     },
     {
@@ -34,8 +26,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 191,
       "vat_rate": 21,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["BE"],
+      "vat_amount": vatMap["BE"],
     },
     {
       "vat_return_supply_id": 0,
@@ -46,8 +38,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 192,
       "vat_rate": 20,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["BG"],
+      "vat_amount": vatMap["BG"],
     },
     {
       "vat_return_supply_id": 0,
@@ -58,8 +50,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 187,
       "vat_rate": 25,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["HR"],
+      "vat_amount": vatMap["HR"],
     },
     {
       "vat_return_supply_id": 0,
@@ -70,8 +62,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 271,
       "vat_rate": 19,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["CY"],
+      "vat_amount": vatMap["CY"],
     },
     {
       "vat_return_supply_id": 0,
@@ -82,8 +74,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 193,
       "vat_rate": 21,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["CZ"],
+      "vat_amount": vatMap["CZ"],
     },
     {
       "vat_return_supply_id": 0,
@@ -94,8 +86,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 195,
       "vat_rate": 25,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["DK"],
+      "vat_amount": vatMap["DK"],
     },
     {
       "vat_return_supply_id": 0,
@@ -106,8 +98,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 299,
       "vat_rate": 22,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["EE"],
+      "vat_amount": vatMap["EE"],
     },
     {
       "vat_return_supply_id": 0,
@@ -118,8 +110,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 197,
       "vat_rate": 24,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["FI"],
+      "vat_amount": vatMap["FI"],
     },
     {
       "vat_return_supply_id": 0,
@@ -130,8 +122,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 220,
       "vat_rate": 20,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["FR"],
+      "vat_amount": vatMap["FR"],
     },
     {
       "vat_return_supply_id": 0,
@@ -142,8 +134,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 199,
       "vat_rate": 19,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["DE"],
+      "vat_amount": vatMap["DE"],
     },
     {
       "vat_return_supply_id": 0,
@@ -154,8 +146,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 225,
       "vat_rate": 24,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["GR"],
+      "vat_amount": vatMap["GR"],
     },
     {
       "vat_return_supply_id": 0,
@@ -166,8 +158,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 201,
       "vat_rate": 27,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["HU"],
+      "vat_amount": vatMap["HU"],
     },
     {
       "vat_return_supply_id": 0,
@@ -178,8 +170,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 202,
       "vat_rate": 23,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["IE"],
+      "vat_amount": vatMap["IE"],
     },
     {
       "vat_return_supply_id": 0,
@@ -190,8 +182,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 203,
       "vat_rate": 22,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["IT"],
+      "vat_amount": vatMap["IT"],
     },
     {
       "vat_return_supply_id": 0,
@@ -202,8 +194,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 205,
       "vat_rate": 21,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["LT"],
+      "vat_amount": vatMap["LT"],
     },
     {
       "vat_return_supply_id": 0,
@@ -214,8 +206,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 207,
       "vat_rate": 18,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["MT"],
+      "vat_amount": vatMap["MT"],
     },
     {
       "vat_return_supply_id": 0,
@@ -226,8 +218,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 208,
       "vat_rate": 21,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["NL"],
+      "vat_amount": vatMap["NL"],
     },
     {
       "vat_return_supply_id": 0,
@@ -238,8 +230,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 254,
       "vat_rate": 23,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["PL"],
+      "vat_amount": vatMap["PL"],
     },
     {
       "vat_return_supply_id": 0,
@@ -250,8 +242,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 210,
       "vat_rate": 23,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["PT"],
+      "vat_amount": vatMap["PT"],
     },
     {
       "vat_return_supply_id": 0,
@@ -262,8 +254,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 211,
       "vat_rate": 19,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["RO"],
+      "vat_amount": vatMap["RO"],
     },
     {
       "vat_return_supply_id": 0,
@@ -274,8 +266,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 214,
       "vat_rate": 21,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["ES"],
+      "vat_amount": vatMap["ES"],
     },
     {
       "vat_return_supply_id": 0,
@@ -286,8 +278,8 @@ x-access-token: {{access_token}}
       "vat_rate_id": 215,
       "vat_rate": 25,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["SE"],
+      "vat_amount": vatMap["SE"],
     },
     {
       "vat_return_supply_id": 0,
@@ -298,12 +290,31 @@ x-access-token: {{access_token}}
       "vat_rate_id": 204,
       "vat_rate": 21,
       "vat_rate_type": "Standard VAT Rate",
-      "taxable_amount": 0.0,
-      "vat_amount": 0.0
+      "taxable_amount": resMap["LV"],
+      "vat_amount": vatMap["LV"],
     }
   ],
   "supplies_from_fixed_establishments": [],
   "vat_corrections": []
 }
 
-HTTP 200
+const uploadRes = await fetch(`https://tax-oss.mof.gov.cy/api/vatreturn/${process.env.RETURN_ID}/save/temporary`, {
+	method: 'PUT',
+	headers: {
+		'Accept': 'application/json, text/plain, */*',
+		'Connection': 'keep-alive',
+		'Referer': `https://tax-oss.mof.gov.cy/vat-return/edit/${process.env.RETURN_ID}`,
+		'Content-Type': 'application/json',
+		'Sec-Fetch-Dest': 'empty',
+		'Sec-Fetch-Mode': 'cors',
+		'Sec-Fetch-Site': 'same-origin',
+		'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.0.0 Safari/537.36',
+		'sec-ch-ua': '"Google Chrome";v="123", "Not:A-Brand";v="8", "Chromium";v="123"',
+		'sec-ch-ua-mobile': '?0',
+		'sec-ch-ua-platform': '"macOS"',
+		'x-access-token': process.env.ACCESS_TOKEN
+	},
+	body: JSON.stringify(taxJSON),
+});
+
+console.log(await uploadRes.json());
